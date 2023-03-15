1b02a274a408079970ba2190e0d154027302f22d
Alex Nash <Alexr.nash@gmail.com>
2023-03-15 03:07:55 -0400

    set up some javascript variables

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var StateBtn = document.getElementById('citySearch')
+var cityName = document.getElementById('cityName')
+var cityTemp = document.getElementById('cityTemp')
+var cityWind = document.getElementById('cityWind')
+var cityHumidity = document.getElementById('cityHum')
+
+
